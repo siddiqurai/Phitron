6405e823c50f01069c8a04c8b9b8e6e420ce430c
Siddiqur Rahman <infobusiness.anay@gmail.com>
2025-10-06 18:43:34 +0600

    Update Files

---
+++ Introduction to Programming/Module-2/if-else/if_else.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+    
+
+    int football;
+    printf("Enter Time : ");
+    scanf("%d", &football);
+
+    if (football >= 5)
+    {
+        printf("Football Kheltay Jabo");
+    }
+    else
+    {
+        printf("Football Kheltay Jabo na");
+    }
+    
+
+    return 0;
+}
