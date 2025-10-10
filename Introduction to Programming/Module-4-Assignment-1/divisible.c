5a0d9de30808a9a241b9d601e301f68cd8cab089
Siddiqur Rahman <infobusiness.anay@gmail.com>
2025-10-10 12:40:20 +0600

    Update Filies

---
+++ Introduction to Programming/Module-4-Assignment-1/divisible.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int N;
+    scanf("%d", &N);
+
+    if (N % 3 == 0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+    return 0;
+}
